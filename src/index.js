7b8e6e2df24a7e176abf3b8f3097e1ddad3b36c9
秦臻 <qz519189636@qq.com>
2019-12-06 21:12:11 +0800

    qz commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const css = import("css-loader!./css/index.css")
+
+const a = 100
+
+console.log(a, css)
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const rimraf = require('rimraf');
+
+rimraf.sync("dist")
+
+
+module.exports = {
+  entry: './src/index',
+  mode: process.env.NODE_ENV,
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
+}
